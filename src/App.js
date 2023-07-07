74878ea6bc16cfaea42538106dab0caae846ddef
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-07 16:17:03 +0300

    feat: add flashcard component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Carousel from "./Carousel";
+import FlashCard from "./FlashCard";
 import "./index.scss";
 
 const data = [
@@ -6,16 +8,31 @@ const data = [
   { term: "term 2", description: "description 2" },
   { term: "term 3", description: "description 3" },
   { term: "term 4", description: "description 4" },
-  { term: "term 5", description: "description 5" },
+  {
+    term: "term 5",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem corrupti omnis eaque quod qui architecto est itaque magni iure, libero voluptate tempora dolorem distinctio error similique dolore accusamus blanditiis quisquam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem corrupti omnis eaque quod qui architecto est itaque magni iure, libero voluptate tempora dolorem distinctio error similique dolore accusamus blanditiis quisquam?
+    `,
+  },
 ];
 
 export default function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
+  function handleFlip() {
+    setIsFlipped((v) => !v);
+  }
+
   return (
     <div className="App">
       <Carousel>
-        <p>Item 1</p>
-        <p>Item 2</p>
-        <p>Item 3</p>
+        {data.map((item) => (
+          <FlashCard
+            key={item.term}
+            data={item}
+            isFlipped={isFlipped}
+            onFlip={handleFlip}
+          />
+        ))}
       </Carousel>
     </div>
   );
