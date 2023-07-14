17de39c0ff5d4bf7fe248e3522e14ec09a706e07
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-14 14:13:24 +0300

    feat: open one dropdown at the time

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Card from "../Card/Card";
 import "./Dropdown.scss";
 
 export default function Dropdown({ className, renderTrigger, menu }) {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   function handleOpen() {
     setIsOpen((v) => !v);
   }
 
+  // only 1 open dropdown at the time
+  useEffect(() => {
+    function callback(e) {
+      if (e.target.closest(".dropdown") !== dropdownRef.current)
+        setIsOpen(false);
+    }
+
+    window.addEventListener("click", callback);
+
+    return () => window.removeEventListener("click", callback);
+  }, []);
+
   return (
-    <div className={`dropdown ${className}`}>
+    <div className={`dropdown ${className}`} ref={dropdownRef}>
       {renderTrigger(handleOpen)}
 
       {isOpen && (
