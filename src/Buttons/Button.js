60da0623a7bd7a495cd2780db689a88f0f7ea82c
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-14 15:42:32 +0300

    refactor: desctructure button props

--- src/Buttons/Button.js
+++ src/Buttons/Button.js
@@ -4,18 +4,16 @@ export default function Button({
   type = "button",
   className = "",
   onClick,
-  children,
   round,
-  disabled,
-  title,
+  children,
+  ...rest
 }) {
   return (
     <button
       type={type}
-      disabled={disabled}
       className={`btn ${round ? `btn--round` : ""} ${className}`}
       onClick={onClick}
-      title={title}
+      {...rest}
     >
       {children}
     </button>
