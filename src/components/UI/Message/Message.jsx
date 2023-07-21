de96b064c780ae636f978b7c8f4a24bb38879ef7
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-21 10:33:29 +0300

    refactor: add css module for Message component

--- src/components/UI/Message/Message.jsx
+++ src/components/UI/Message/Message.jsx
@@ -1,8 +1,10 @@
 import "./Message.scss";
 
+import styles from "./Message.module.css";
+
 function Message({ message, isError }) {
   return (
-    <p className="message">
+    <p className={styles.message}>
       <span>{isError ? "❌" : "👋"}</span>
       <span>{message}</span>
     </p>
