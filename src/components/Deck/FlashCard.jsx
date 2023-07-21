4c5d25a4944812aa6ab2adacda5f1cda7de0d5f4
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-07-21 17:11:00 +0300

    refactor: change flashcard styles

--- src/components/Deck/FlashCard.jsx
+++ src/components/Deck/FlashCard.jsx
@@ -3,15 +3,23 @@ import Card from "../UI/Card/Card";
 import styles from "./FlashCard.module.css";
 
 export default function FlashCard({ data, isFlipped, onFlip }) {
-  const { term, description } = data;
+  const { term, description, image } = data;
+
+  const descriptionSide = (
+    <>
+      <p className={styles.description}>{description}</p>
+      {image?.small && <img className={styles.image} src={image.small} />}
+    </>
+  );
+
+  const termSide = <p className={styles.term}>{term}</p>;
 
   return (
     <Card className={styles.flashcard} onClick={onFlip}>
       <h3 className={styles.title}>{isFlipped ? "Description" : "Term"}</h3>
-      <p className={isFlipped ? styles.description : styles.term}>
-        {isFlipped ? description : term}
-      </p>
-
+      <div className={styles.content}>
+        {isFlipped ? descriptionSide : termSide}
+      </div>
       <span className={styles.hint}>Click to flip</span>
     </Card>
   );
